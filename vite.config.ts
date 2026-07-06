64108ce02ced4d69d78dda9ee9e9aa692c30dc52
Quantumdraft <info.quantumdraft@gmail.com>
2026-07-06 15:44:10 +0530

    adding the updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
   server: {
+    host: "::",
+    port: 8080,
     allowedHosts: [
-      'quantumdraft-yz2c.onrender.com'
-    ]
-  }
-})
+      "quantumdraft-yz2c.onrender.com",
+    ],
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
